57c2668085a0fda8e552bfdd331119d41c559dcb
Nadar.Murugan <nadar.murugan@ril.com>
2025-11-22 00:02:12 +0530

    removed app = in firebase.js as it is not needed just initialize function trigger is sufficient

--- src/hooks/useFetchMovieTrailer.js
+++ src/hooks/useFetchMovieTrailer.js
@@ -4,10 +4,6 @@ import { API_OPTIONS } from "../utils/constants";
 const useFetchMovieTrailer = (movieId) => {
     const [trailer, setTrailer] = useState(null);
 
-    useEffect(() => {
-        fetchMovieTrailer();
-    }, [movieId, fetchMovieTrailer]);
-
     const fetchMovieTrailer = useCallback(async () => {
         try {
             const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`, API_OPTIONS);
@@ -18,7 +14,11 @@ const useFetchMovieTrailer = (movieId) => {
         } catch (error) {
             console.error("Error fetching movie trailer:", error);
         }
-    }, [trailer])
+    }, [movieId])
+
+    useEffect(() => {
+        fetchMovieTrailer();
+    }, [movieId, fetchMovieTrailer]);
 
     return trailer;
 }
